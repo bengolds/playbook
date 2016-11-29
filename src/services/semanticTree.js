67de6c497e887efeeec0bac29a107fc781f75618
Ben Goldsmith <theotherben@gmail.com>
2016-11-28 21:16:18 -0800

    Tree supports addition, multiplication, subtraction

--- src/services/semanticTree.js
+++ src/services/semanticTree.js
@@ -1,6 +1,13 @@
 Controller.open(function(_) {
   _.exportSemanticTree = function() {
-    return this.root.toSemanticTree();
+    var semanticTree = this.root.toSemanticTree();
+    return semanticTree.toString();
+  };
+  _.logSemanticTree = function() {
+    console.log(this.root.toSemanticTree());
+  };
+  _.logDisplayTree = function() {
+    console.log(this.root);
   };
 });
 
@@ -25,13 +32,21 @@ var FunctionNode = P(SemanticNode, function(_, super_) {
     this.args = args;
     super_.init.call(this);
   };
+  _.toString = function () {
+    var ret = '(' + this.args[0].toString() + this.operator.toString() + this.args[1].toString() + ')';
+    return ret;
+  }
 });
 
 var SymbolNode = P(SemanticNode, function(_, super_) {
   _.__type__ = "SymbolNode";
-  _.init = function (symbol) {
+  _.init = function (symbol, rightAssociative) {
     this.symbol = symbol;
+    this.rightAssociative = rightAssociative;
     super_.init.call(this);
+  }
+  _.toString = function () {
+    return this.symbol;
   }
 });
 
@@ -42,6 +57,9 @@ var VariableNode = P(SemanticNode, function(_, super_) {
     this.variableType = variableType || SETS.REAL;
     super_.init.call(this);
   }
+  _.toString = function() {
+    return this.variableName;
+  }
 });
 
 
@@ -51,11 +69,115 @@ Fraction.open(function(_) {
   };
 });
 
+
+
+function comparePrecedences(o1, o2) {
+  var precedences = {
+    '*' : 2,
+    '/' : 2,
+    '+' : 1,
+    '-' : 1
+  }
+  var p1 = precedences[o1.symbol];
+  var p2 = precedences[o2.symbol];
+  if (p1 > p2) {
+    return 1;
+  } else if (p1 == p2) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+function canStack(o1, o2) {
+  //return true if o1 can stack on o2
+  var compPrec = comparePrecedences(o1, o2);
+  if (!o1.rightAssociative) 
+    return compPrec > 0;
+  else 
+    return compPrec >= 0;
+}
+
+function addNode(operandStack, operator) {
+  //Combine the top two operands and the operator into one node,
+  //then push it to the top of the stack.
+  var rNode = operandStack.pop(), lNode = operandStack.pop();
+  var args = [lNode, rNode];
+  operandStack.push(FunctionNode(operator, args));
+}
+
+function isOperand(semanticNode) {
+  return !(semanticNode instanceof SymbolNode);
+}
+
+function peekBack(stack) {
+  return stack[stack.length-1];
+}
+
 Node.open(function(_) {
   _.toSemanticTree = function() {
-    var output = "";
-    return this.foldChildren(output, function(text, child) {
-      return text + child.toSemanticTree();
-    });
+    var operandStack = [];
+    var operatorStack = [];
+    var tree = {};
+
+
+    var currNode = this.ends[L];
+    var prevNode = null;
+
+    while(currNode) {
+      currSemanticNode = currNode.toSemanticTree();
+      if (isOperand(currSemanticNode)) {
+        operandStack.push(currSemanticNode);
+      } 
+      else {
+        //If we're trying to stack a low-precedence operator on top of a 
+        //high precedence operator, pop operators until we can fit ours.
+        while (operatorStack.length > 0 &&
+              !canStack(currSemanticNode, peekBack(operatorStack))) 
+        { 
+          addNode(operandStack, operatorStack.pop());
+        }
+        operatorStack.push(currSemanticNode); 
+      }
+      prevNode = currNode;
+      currNode = currNode[R];
+    }
+
+    while(operatorStack.length > 0) {
+      addNode(operandStack, operatorStack.pop());
+    }
+
+    console.log(operandStack);
+    console.log(operatorStack);
+    return operandStack[0];
   };
+});
+
+BinaryOperator.open(function(_) {
+  _.toSemanticTree = function () {
+    var symbol = '!';
+    var rightAssociative = false;
+    switch (this.ctrlSeq) {
+      case '+':
+        symbol = '+';
+        break;
+      case '-':
+        symbol = '-';
+        break;
+      case '\\div ':
+        symbol = '/';
+        break;
+      case '\\cdot ':
+      case '\\times ':
+        symbol = '*';
+        break;
+    }
+    return SymbolNode(symbol, rightAssociative);
+  }
+});
+
+Variable.open(function(_) {
+  _.toSemanticTree = function () {
+    return VariableNode(this.ctrlSeq);
+  }
 });
