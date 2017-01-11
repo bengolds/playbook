f1e281457708f9630460aa35224e91002f526cf1
Ben Goldsmith <theotherben@gmail.com>
2017-01-10 22:38:32 -0800

    added ghpages script

---
+++ to-ghpages.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/zsh
+ORIG_PATH=$(/bin/readlink -f ${0%/*})
+cd ..
+mkdir temp && cd temp
+git clone "git@github.com:bengolds/playbook.git" page --single-branch --branch gh-pages
+
+cd page
+
+FILES=(bower_components libs resources src _config.yml index.html Gemfile)
+git rm -rf -q .
+
+cd $ORIG_PATH
+echo $PWD
+cp -r $FILES ../temp/page
+
+cd ../temp/page
+git add -A .
+git commit -am "Deploy to GitHub Pages"
+git push --force --quiet
+
+cd ../../
+rm -rf temp
+
